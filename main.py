b757b527ac22401d0c8e278cc177b805c569016f
XEROXV23 <carlos.vidal0493@gmail.com>
2022-10-26 21:54:59 -0500

    flask_bootstrap

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from ensurepip import bootstrap
 from flask import Flask, request, make_response, redirect, render_template
-
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 todos = ['Comprar café', 'Enviar solicitud de compra', 'Entregar video al productor']
 
