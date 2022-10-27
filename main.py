7a25bfb9500abdbc3d63f472b88419f8b367edd7
XEROXV23 <carlos.vidal0493@gmail.com>
2022-10-26 21:16:53 -0500

    Error 404

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 todos = ['Comprar café', 'Enviar solicitud de compra', 'Entregar video al productor']
 
 
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error=error)
+
+
 @app.route('/')
 def index():
     user_ip = request.remote_addr
