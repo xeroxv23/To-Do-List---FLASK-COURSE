324f47e80bd319e3cd7a7f53ca6f3eec03896e8e
XEROXV23 <carlos.vidal0493@gmail.com>
2022-11-02 18:46:05 -0600

    blueprints_2

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from flask import request, make_response, redirect, render_template, session, url_for, flash
 import unittest
 
@@ -7,7 +6,8 @@ from app.forms import LoginForm
 
 app = create_app()
 
-todos = ['Comprar café', 'Enviar solicitud de compra', 'Entregar video al productor']
+
+todos = ['Comprar cafe', 'Enviar solicitud de compra', 'Entregar video a productor ']
 
 
 @app.cli.command()
@@ -20,8 +20,9 @@ def test():
 def not_found(error):
     return render_template('404.html', error=error)
 
+
 @app.errorhandler(500)
-def internal_server_error(error):
+def server_error(error):
     return render_template('500.html', error=error)
 
 
@@ -31,31 +32,19 @@ def index():
 
     response = make_response(redirect('/hello'))
     session['user_ip'] = user_ip
-    
 
     return response
 
 
-@app.route('/hello', methods=['GET', 'POST'])
+@app.route('/hello', methods=['GET'])
 def hello():
     user_ip = session.get('user_ip')
-    login_form = LoginForm()
     username = session.get('username')
 
     context = {
-        'user_ip' : user_ip,
-        'todos' : todos,
-        'login_form' : login_form,
-        'username' : username
+        'user_ip': user_ip,
+        'todos': todos,
+        'username': username
     }
-
-    if login_form.validate_on_submit():
-        username = login_form.username.data
-        session['username'] = username
-
-        flash('Nombre de usuario registrado con exito!')
-
-        return redirect(url_for('index'))
-
 
     return render_template('hello.html', **context)
