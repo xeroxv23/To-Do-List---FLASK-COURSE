313a574df4ba358603ffe58dcb66a3e1645edf2a
XEROXV23 <carlos.vidal0493@gmail.com>
2022-11-03 07:35:39 -0600

    Agregamos lista de tareas a nuevos usuarios

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from flask_login import UserMixin
 
 from .firestore_service import get_user
 
+
 class UserData:
     def __init__(self, username, password):
         self.username = username
@@ -16,12 +17,12 @@ class UserModel(UserMixin):
         self.id = user_data.username
         self.password = user_data.password
 
-        @staticmethod
-        def query(user_id):
-            user_doc = get_user(user_id)
-            user_data = UserData(
-                username=user_doc.id,
-                password=user_doc.to_dict()['password']
-            )
+    @staticmethod
+    def query(user_id):
+        user_doc = get_user(user_id)
+        user_data = UserData(
+            username=user_doc.id,
+            password=user_doc.to_dict()['password']
+        )
 
-            return UserModel(user_data)
+        return UserModel(user_data)
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import unittest
 from flask import request, make_response, redirect, render_template, session, url_for, flash
-from flask_login import login_required
+from flask_login import login_required, current_user
+
 
 from app import create_app
-from app.forms import LoginForm
-from app.firestore_service import get_users, get_todos
+from app.forms import TodoForm
+from app.firestore_service import get_users, get_todos, put_todo
 
 app = create_app()
 
@@ -35,16 +36,27 @@ def index():
     return response
 
 
-@app.route('/hello', methods=['GET'])
+@app.route('/hello', methods=['GET', 'POST'])
 @login_required
 def hello():
     user_ip = session.get('user_ip')
-    username = session.get('username')
+    username = current_user.id
+    todo_form = TodoForm()
 
     context = {
         'user_ip': user_ip,
-        'todos' : get_todos(user_id=username),
-        'username': username
+        'todos': get_todos(user_id=username),
+        'username': username,
+        'todo_form': todo_form
+
     }
-    
+
+    if todo_form.validate_on_submit():
+        put_todo(user_id=username, description=todo_form.description.data)
+
+        flash('Tu tarea se creo con éxito!')
+
+        return redirect(url_for('hello'))
+
     return render_template('hello.html', **context)
+
