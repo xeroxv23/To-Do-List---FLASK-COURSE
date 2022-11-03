52576b09bcafe73c42c8a6cc9e5f870c76a5016e
XEROXV23 <carlos.vidal0493@gmail.com>
2022-11-02 22:34:50 -0600

    Auteticacion de usarios LOGIN

--- app/firestore_service.py
+++ app/firestore_service.py
@@ -1,3 +1,4 @@
+
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
@@ -10,6 +11,11 @@ db = firestore.client()
 
 def get_users():
     return db.collection('users').get()
+
+
+def get_user(user_id):
+    return db.collection('users').document(user_id).get()
+
 
 def get_todos(user_id):
     return db.collection('users')\
