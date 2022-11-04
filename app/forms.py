e8bf32770c6b57e9a8b45f44756af065f347fd47
XEROXV23 <carlos.vidal0493@gmail.com>
2022-11-03 22:05:42 -0600

    Delete_tareas

--- app/forms.py
+++ app/forms.py
@@ -12,3 +12,6 @@ class LoginForm(FlaskForm):
 class TodoForm(FlaskForm):
     description = StringField('Descripción', validators=[DataRequired()])
     submit = SubmitField('Crear')
+
+class DeleteTodoForm(FlaskForm):
+    submit = SubmitField('Borrar')
